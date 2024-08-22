53488a01ac7e0637916a3cdc147c93e18434309d
Damndaniel241 <lombardia241@gmail.com>
2024-08-22 03:31:18 -0700

    connected to signup enpoint

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,4 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref,onBeforeMount, onMounted } from 'vue';
+import axios from 'axios';
+
+const username = ref<string>("");
+const email = ref<string>("");
+const password = ref<string>("");
+
+
+async function register(){
+    const formData = {
+        username:username.value,
+        email:email.value,
+        password:password.value
+    }
+
+    try {
+        const response = await axios.post("http://127.0.0.1:8000/accounts/register/", formData);
+        return response.data;
+    } catch (error) {
+        console.error("An error occurred during registration:", error);
+        // Handle the error as needed, e.g., return an error message or null
+        return null;
+    }
+}
+
+// onMounted(async()=>{
+//     register();
+// }) 
+
+
+</script>
 
 <template>
   <div
@@ -9,27 +40,46 @@
   <div
     class="bg-[#e8ece0] rounded-lg w-[70em] place-self-center flex flex-col border border-gray-300 shadow-lg"
   >
-   
-    <div class="bg-[#F6F6EC] p-2 flex justify-center gap-2 border border-gray-300 rounded-t-lg">
-        <div class="flex items-center gap-1">
+    <div
+      class="bg-[#F6F6EC] p-2 flex justify-center gap-2 border border-gray-300 rounded-t-lg"
+    >
+      <div class="flex items-center gap-1">
         <span>Email: </span>
-        <input type="email" class="p-1 rounded-xl bg-white border-gray-300" />
+        <input
+          type="email"
+          v-model="email"
+          class="p-1 rounded-xl bg-white border-gray-300"
+        />
       </div>
-        </div>
-        <div class="bg-[#e8ece0] p-2 flex justify-center gap-2  border border-gray-300">
-        <div class="flex items-center gap-1">
+    </div>
+    <div
+      class="bg-[#e8ece0] p-2 flex justify-center gap-2 border border-gray-300"
+    >
+      <div class="flex items-center gap-1">
         <span>Username: </span>
-        <input type="text" class="p-1 rounded-xl bg-white border-gray-300" />
+        <input
+          type="text"
+          v-model="username"
+          class="p-1 rounded-xl bg-white border-gray-300"
+        />
       </div>
-        </div>
-        <div class="bg-[#F6F6EC] p-2 flex justify-center gap-2 ">
-        <div class="flex items-center gap-1">
+    </div>
+    <div class="bg-[#F6F6EC] p-2 flex justify-center gap-2">
+      <div class="flex items-center gap-1">
         <span>Password: </span>
-        <input type="password" class="p-1 rounded-xl bg-white border-gray-300" />
+        <input
+          type="password"
+          v-model="password"
+          class="p-1 rounded-xl bg-white border-gray-300"
+        />
       </div>
-        </div>
-        <div class="bg-[#e8ece0] p-2 flex justify-center gap-2 rounded-b-lg border border-gray-300">
-        <button class="bg-white rounded-2xl p-2 cursor-pointer">join firekat</button>
-        </div>
+    </div>
+    <div
+      class="bg-[#e8ece0] p-2 flex justify-center gap-2 rounded-b-lg border border-gray-300"
+    >
+      <button class="bg-white rounded-2xl p-2 cursor-pointer" @click="register">
+        join firekat
+      </button>
+    </div>
   </div>
 </template>
